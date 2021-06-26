145c978d3b67e37bb1a138750b7f09e14aeffbd9
SHASHANK <170030159@kluniversity.in>
2021-06-26 15:01:47 +0530

    restTemplate instantiation- @bean, appended movieinfo url with movie id

--- MovieCatalog/src/main/java/com/vm/springboot/ms/moviecatalog/MovieCatalogApplication.java
+++ MovieCatalog/src/main/java/com/vm/springboot/ms/moviecatalog/MovieCatalogApplication.java
@@ -2,6 +2,8 @@ package com.vm.springboot.ms.moviecatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogApplication {
@@ -9,5 +11,8 @@ public class MovieCatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
--- MovieCatalog/src/main/java/com/vm/springboot/ms/moviecatalog/resource/MovieCatalogResource.java
+++ MovieCatalog/src/main/java/com/vm/springboot/ms/moviecatalog/resource/MovieCatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,16 @@ import com.vm.springboot.ms.moviecatalog.model.Movie;
 import com.vm.springboot.ms.moviecatalog.model.Rating;
 
 
-
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-	
+	@Autowired
+	RestTemplate restTemplate;
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		//1. get all the rated movie ids freom the ratings service
-		RestTemplate restTemplate = new RestTemplate();
 
 		List<Rating> ratingsBag = Arrays.asList(
 				new Rating("1234",4),
@@ -35,8 +36,8 @@ public class MovieCatalogResource {
 		//for each movie id retrieved from the previous MS call send it to MovieInfo and get movie details
 		return ratingsBag.stream()                                                  //putting the list of items [emptying the bag on a] conveyor belt
 				.map(rating -> {
-					Movie movie = restTemplate.getForObject("http://localhost:8083/movies/shashank", Movie.class);
-					return new CatalogItem(movie.getMovieName(), "description", rating.getRating());              				   //converting each rating item moving on the conveyor belt TO  catalogitem
+					Movie movie = restTemplate.getForObject("http://localhost:8083/movies/"+rating.getMovieId(), Movie.class);
+					return new CatalogItem(movie.getMovieName(), "description"+movie.getMovieId(), rating.getRating());              				   //converting each rating item moving on the conveyor belt TO  catalogitem
    
 				})
 				.collect(Collectors.toList());                                                                                     //after all the items have been converted put it back into the bag[list]
